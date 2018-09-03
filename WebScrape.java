ee692f27d13cfe3ae12e937e925f2d252eb08fad
ShaneLee <shaneleephoto@gmail.com>
2018-09-03 19:10:25 +0100

    Add files via upload

---
+++ WebScrape.java
@@ -0,0 +1,47 @@
+package webscrape;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ *
+ * @author Shane - shanelee.co.uk
+*/
+
+
+public class WebScrape {
+    public static void main(String[] args) {
+        final String url = 
+                "https://shares.telegraph.co.uk/indices/summary/index/MCX";
+        
+        try {
+            final Document document = Jsoup.connect(url).get();
+            
+            for (Element row : document.select(
+                "table.tablesorter.full tr")) {
+                if (row.select("td:nth-of-type(1)").text().equals("")) {
+                    continue;
+                }
+                else {
+                   final String ticker = 
+                           row.select("td:nth-of-type(1)").text();
+                   final String name = 
+                           row.select("td:nth-of-type(2)").text();
+                   final String tempPrice = 
+                           row.select("td.right:nth-of-type(3)").text();
+                   final String tempPrice1 = 
+                           tempPrice.replace(",", "");
+                   final double price = Double.parseDouble(tempPrice1);
+                   
+                    System.out.println(ticker + " " + name + " " + price);
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+}
